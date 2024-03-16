e9de7c6b05c9d1a32a55738e0693014e32b98d2d
Anuj Shah <anujshah584@gmail.com>
2024-03-16 19:08:07 +0530

    data path exists check

--- fetch_data.py
+++ fetch_data.py
@@ -1,19 +1,25 @@
-# fetch_data.py
 from jugaad_data.nse import NSELive
 import json
 from datetime import datetime
+import os  # Import os module
 
 
 def fetch_and_save_stock_data(symbol):
     nse_live = NSELive()
     stock_quote = nse_live.stock_quote(symbol)
-    filename = f"data/{symbol}_{datetime.now().strftime('%Y-%m-%d')}.json"
+
+    # Ensure 'data' directory exists
+    data_directory = "data"
+    if not os.path.exists(data_directory):
+        os.makedirs(data_directory)  # Create the 'data' directory if it does not exist
+
+    filename = f"{data_directory}/{symbol}_{datetime.now().strftime('%Y-%m-%d')}.json"
     with open(filename, "w") as file:
         json.dump(stock_quote, file)
     print(f"Data for {symbol} saved to {filename}")
 
 
 if __name__ == "__main__":
-    symbols = ["SBIN", "RELIANCE", "TCS"]
+    symbols = ["SBIN", "RELIANCE", "TCS"]  # Add your symbols here
     for symbol in symbols:
         fetch_and_save_stock_data(symbol)
